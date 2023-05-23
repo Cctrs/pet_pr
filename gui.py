e7fb1fa37624a74088f413d47f2eb28fa93e663d
cctrs <agrosmaker@gmail.com>
2023-05-24 02:02:19 +0300

    remove print check, add exit button

--- gui.py
+++ gui.py
@@ -9,19 +9,19 @@ input_box = PySimpleGUI.InputText(tooltip='Enter task here', key='task')
 button_add = PySimpleGUI.Button('Add task')
 button_edit = PySimpleGUI.Button('Edit task')
 button_done = PySimpleGUI.Button('Done!')
+button_exit = PySimpleGUI.Button('Exit')
 tasks_list = PySimpleGUI.Listbox(values=functions.get_tasks(), key='tasks_item',
                                  enable_events=True, size=(100, 20))
 
 start_window = PySimpleGUI.Window('Tasks',
                                   layout=[[text1, input_box, button_add],
                                           [tasks_list], [button_edit],
-                                          [button_done]],
+                                          [button_done],
+                                          [button_exit]],
                                   font=('Ariel', 10))
 
 while True:
     event, values = start_window.read()
-    print(event)
-    print(values)
     match event:
         case 'Add task':
             tasks = functions.get_tasks()
@@ -50,6 +50,9 @@ while True:
             tasks.remove(task_done)
             functions.write_tasks(tasks)
             start_window['tasks_item'].update(values=tasks)
+            start_window['task'].update('')
+        case 'Exit':
+            break
         case PySimpleGUI.WIN_CLOSED:
             break
 
