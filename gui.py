a9f912994f33109d5857c087d23b647b3c78bda8
cctrs <agrosmaker@gmail.com>
2023-05-24 03:04:22 +0300

    add theme, handle some errors

--- gui.py
+++ gui.py
@@ -4,6 +4,8 @@ import PySimpleGUI
 
 import functions
 
+PySimpleGUI.theme("DarkGreen3")
+
 text1 = PySimpleGUI.Text('Enter your task: ')
 input_box = PySimpleGUI.InputText(tooltip='Enter task here', key='task')
 button_add = PySimpleGUI.Button('Add task')
@@ -31,26 +33,32 @@ while True:
             start_window['tasks_item'].update(values=tasks)
             start_window['task'].update('')
         case 'Edit task':
-            edit_task = values['tasks_item'][0]
-            new_task = values['task']
-            tasks = functions.get_tasks()
-            index = tasks.index(edit_task)
-            tasks[index] = new_task + '\n'
-            functions.write_tasks(tasks)
-            start_window['tasks_item'].update(values=tasks)
+            try:
+                edit_task = values['tasks_item'][0]
+                new_task = values['task']
+                tasks = functions.get_tasks()
+                index = tasks.index(edit_task)
+                tasks[index] = new_task + '\n'
+                functions.write_tasks(tasks)
+                start_window['tasks_item'].update(values=tasks)
+            except IndexError:
+                PySimpleGUI.Popup('Please, select the task to edit.', title="Edit Window")
         case 'tasks_item':
             start_window['task'].update(value=values['tasks_item'][0])
         case 'Done!':
-            task_done = values['tasks_item'][0]
-            done_tasks_doc = functions.get_done_tasks()
-            date_done = time.strftime("%Y-%B-%d %H:%M")
-            done_tasks_doc.append(task_done + date_done + '\n')
-            functions.write_done_tasks(done_tasks_doc)
-            tasks = functions.get_tasks()
-            tasks.remove(task_done)
-            functions.write_tasks(tasks)
-            start_window['tasks_item'].update(values=tasks)
-            start_window['task'].update('')
+            try:
+                task_done = values['tasks_item'][0]
+                done_tasks_doc = functions.get_done_tasks()
+                date_done = time.strftime("%Y-%B-%d %H:%M")
+                done_tasks_doc.append(task_done + date_done + '\n')
+                functions.write_done_tasks(done_tasks_doc)
+                tasks = functions.get_tasks()
+                tasks.remove(task_done)
+                functions.write_tasks(tasks)
+                start_window['tasks_item'].update(values=tasks)
+                start_window['task'].update('')
+            except IndexError:
+                PySimpleGUI.Popup('Please, select the completed task.', title='Complete Window')
         case 'Exit':
             break
         case PySimpleGUI.WIN_CLOSED:
